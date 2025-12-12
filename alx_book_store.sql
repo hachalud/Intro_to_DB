99d89d6697ae9aab876bae3407cff7b630af18e3
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-12-12 22:11:14 +0300

    db create

---
+++ alx_book_store.sql
@@ -0,0 +1,30 @@
+-- create db name alx_book_store
+CREATE DATABASE alx_book_store;
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE
+);
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    name VARCHAR(255),
+);
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE orders (
+    order_id (Primary Key)
+    customer_id (Foreign Key referencing Customers table)
+    order_date DATE
+);
+CREATE TABLE order_details (
+    orderdetailid (Primary Key)
+    order_id (Foreign Key referencing Orders table)
+    book_id (Foreign Key referencing Books table)
+    quantity DOUBLE
+);
