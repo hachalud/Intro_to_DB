bda496c19e2934cd38a25eec89ef575e7f949c45
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-12-14 20:28:52 +0300

    origin main

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,16 @@
 import mysql.connector
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="Alx_db",
-    password="Alx_db",
-    database="Alx_db"
+    password="Alx_db"
 )
 
-print(mydb.get_server_info())
+cursor = mydb.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+print("Database created successfully")
+
+cursor.close()
+mydb.close()
