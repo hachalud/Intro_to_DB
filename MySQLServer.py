6c0487ecc0fd974ace906e909cc039b8fd256642
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-12-14 20:31:48 +0300

    sript

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,16 +1,17 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="Alx_db",
-    password="Alx_db"
-)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="Alx_db",
+        password="Alx_db"
+    )
 
-cursor = mydb.cursor()
+    cursor = mydb.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    cursor.close()
+    mydb.close()
 
-print("Database created successfully")
-
-cursor.close()
-mydb.close()
+except mysql.connector.Error as err:
+    print(err)
